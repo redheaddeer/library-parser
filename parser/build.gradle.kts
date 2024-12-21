7ad7ab4f4638950fb15ad5acc6982afe07b7ccd2
Ксения Тапок <soulssfg@gmail.com>
2024-12-21 13:51:01 +0300

    init project

---
+++ parser/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("kotlin")
+    id("idea")
+}
+
+group = "red.head.deer"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ parser/src/main/java/red/head/deer/Main.java
@@ -0,0 +1,7 @@
+package red.head.deer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
